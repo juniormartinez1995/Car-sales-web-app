9c7aa13a25d3e03fd5e2b781bdf81af14e7b5629
Alfonso Martinez <engmartinez00@gmail.com>
2018-08-15 12:03:35 -0300

    Add controller tabela FIPE, View das marcas, etc..

---
+++ SitePI/Controllers/FipeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SitePI.Models.FipeAPI;
+
+namespace SitePI.Controllers
+{
+    public class FipeController : Controller
+    {
+        public IActionResult Marcas()
+        {
+
+            var m = new Marcas();
+            var ListaMrc = m.GetJsonData<Marcas>("http://fipeapi.appspot.com/api/1/carros/marcas.json");
+            return View(ListaMrc);
+        }
+    }
+}
